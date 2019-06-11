af535a8f6cb5a63c1c444de3edfa5ca654e0ec88
lizziemac <liz@stratisiot.com>
2019-06-11 09:17:58 -0400

    Added script for quick update of files

---
+++ gitUpdate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp ~/.vimrc myVimRC
+cp ~/.bash_profile myBashProfile
+
+git add myBashProfile myVimRC
+
+THEDATE=$(date +"%d.%m.%Y")
+git commit -m "$THEDATE Standard refresh of vimrc and bash_profile"
+
+git push
